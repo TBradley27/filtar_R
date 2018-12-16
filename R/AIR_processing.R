cb5fa4e40d802b70daa88c69aa4af04d1fd5013b
TBradley27 <thomas.bradley435@gmail.com>
2018-12-16 19:06:48 +0000

    no longer remove transcript version information when preparing AIR files

--- R/AIR_processing.R
+++ R/AIR_processing.R
@@ -7,7 +7,5 @@ get_canonical_AIRs = function(utr_lengths) {
 	AIRs$AIR = 100
 	AIRs = AIRs[,c('tx_id','start','utr_length','AIR')]
 
-	AIRs$tx_id = gsub('\\.[0-9][0-9]?','',AIRs$tx_id) # remove version number - we may change this later
-
 	return(AIRs)
 }
