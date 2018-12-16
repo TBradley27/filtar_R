cfa98c4d2d9f8910b977618a36b7f647f29f301d
TBradley27 <thomas.bradley435@gmail.com>
2018-12-16 16:55:05 +0000

    include salmon averaging function

--- R/average_dataframes.R
+++ R/average_dataframes.R
@@ -24,3 +24,29 @@ df$avg = avg
 
 return(df)
 }
+
+#' Get the average abundance per records for multiple salmon runs
+
+#' @param df A merged salmon dataframe in which each record represent one locus on a give chromosome, and columns represent coverage values from different read quasi-mapping analyses
+#' @return df A bedgraph dataframe with the artihmetic mean average coverage for each records locus
+#' @export
+
+AvgSalmonQuant = function (df) {
+
+avg = vector(length=dim(df)[1])
+
+# get the average of col_4 ... col_n
+
+cov_values = df[,2:dim(df)[2]]
+cov_values = t(cov_values)
+
+for (i in 1:dim(cov_values)[2]) {
+
+        # only retrieve the bed record columns
+        avg[i] = mean(cov_values[,i])
+}
+
+df$avg = avg
+
+return(df)
+}
