c9e6ed0add5acec49f039eedade815b840c1f392
TBradley27 <thomas.bradley435@gmail.com>
2018-12-20 23:09:50 +0000

    add fix_ts_output test

---
+++ tests/testthat/test_fix_ts_output.R
@@ -0,0 +1,14 @@
+library(plyr)
+library(tidyverse)
+library(filtar)
+
+context('test fix_ts_output')
+
+output_canonical=read_tsv('example_targetscan_sites.tsv') # this is output from the unpatched script
+output = fix_ts_output('example_targetscan_sites_intermediate.tsv')            #('tmp.tsv') # this is output from patched script
+
+output_canonical[is.na(output_canonical)] <- ""
+
+test_that('Canonical and patched script output is identical',{
+        expect_equal(output, output_canonical)
+})
